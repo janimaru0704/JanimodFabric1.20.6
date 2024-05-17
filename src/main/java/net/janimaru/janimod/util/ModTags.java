677f05a0c372a84f7f3ba529688aa08238953892
janimaru0704 <sod0704.225@gmail.com>
2024-05-17 23:33:27 +0900

    タグ宣言用クラス追加

---
+++ src/main/java/net/janimaru/janimod/util/ModTags.java
@@ -0,0 +1,25 @@
+package net.janimaru.janimod.util;
+
+import net.janimaru.janimod.Janimod;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+
+public class ModTags {
+    public static class Blocks {
+        public static final TagKey<Block> DETECTABLE_ORES = createTag("detectable_ores");
+
+        private static TagKey<Block> createTag(String name) {
+            return TagKey.of(RegistryKeys.BLOCK, new Identifier(Janimod.MOD_ID, name));
+        }
+    }
+
+    public static class Items {
+
+        private static TagKey<Item> createTag(String name) {
+            return TagKey.of(RegistryKeys.ITEM, new Identifier(Janimod.MOD_ID, name));
+        }
+    }
+}
