a246a3c2ba9c951faaa4bac474d9ff4d4f68d89b
janimaru0704 <sod0704.225@gmail.com>
2024-05-17 15:54:20 +0900

    アイテム janimod:ruby 追加

--- src/main/java/net/janimaru/janimod/Janimod.java
+++ src/main/java/net/janimaru/janimod/Janimod.java
@@ -2,6 +2,7 @@ package net.janimaru.janimod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.janimaru.janimod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class Janimod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
