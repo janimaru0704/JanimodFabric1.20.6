0a6b5d2748a78b455b9b53a03120b6a881ea1f3e
janimaru0704 <sod0704.225@gmail.com>
2024-05-18 10:13:33 +0900

    カエデの原木と木の樹皮を剥けるようにする カエデ関連ブロックが燃えるようにする

--- src/main/java/net/janimaru/janimod/Janimod.java
+++ src/main/java/net/janimaru/janimod/Janimod.java
@@ -2,9 +2,12 @@ package net.janimaru.janimod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
+import net.fabricmc.fabric.api.registry.StrippableBlockRegistry;
 import net.janimaru.janimod.block.ModBlocks;
 import net.janimaru.janimod.item.ModItemGroups;
 import net.janimaru.janimod.item.ModItems;
+import net.janimaru.janimod.util.ModTags;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,5 +21,12 @@ public class Janimod implements ModInitializer {
 
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
+
+		StrippableBlockRegistry.register(ModBlocks.MAPLE_LOG, ModBlocks.STRIPPED_MAPLE_LOG);
+		StrippableBlockRegistry.register(ModBlocks.MAPLE_WOOD, ModBlocks.STRIPPED_MAPLE_WOOD);
+
+		FlammableBlockRegistry.getDefaultInstance().add(ModTags.Blocks.MAPLE_LOGS, 5, 5);
+		FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.MAPLE_LEAVES, 30, 60);
+		FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.MAPLE_PLANKS, 5, 20);
 	}
 }
